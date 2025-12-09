a2f9b03e2f4a7e55b97ed68c4f3bc05bb34caf8b
mr.Abdrahimov <mr.abdrahimov@gmail.com>
2025-12-10 02:13:55 +0500

    1

---
+++ run_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -d session.madeline ]; then
+    echo "First run"
+    docker run -ti -v ${PWD}:/app hub.madelineproto.xyz/danog/madelineproto:latest php /app/online.php
+    exit 0
+fi
+
+docker run --restart always  -d --name telegram_online -v ${PWD}:/app hub.madelineproto.xyz/danog/madelineproto:latest php /app/online.php
+exit 0
